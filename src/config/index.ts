113221acee5a51ca6dad534fc95d166372e0c717
AlfieriChou <alfierichou@gmail.com>
2018-12-10 20:02:59 +0800

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,13 +2,16 @@ import * as development from './default.config'
 import * as prod from './prod.config'
 import * as _ from 'lodash'
 
-const env: string = process.env.NODE_ENV
+const env: string = process.env.NODE_ENV || 'development'
+const configs: Object = {
+  development: development.config,
+  production: prod.config
+}
+
+const defaultConfig: Object = {
+  env: env,
+  appRoot: process.env.PWD
+}
 
 export let config: Object
-if (env === 'development' || '' || 'undefined') {
-  config = development.config
-}
-if (env === 'production') {
-  config = prod.config
-}
-config = _.merge(config, { env: env })
+config = _.merge(defaultConfig, configs[env])
